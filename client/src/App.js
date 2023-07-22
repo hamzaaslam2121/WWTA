b1f3af69c1045a66ef321df355ae958e52acb3ba
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2023-07-22 20:08:02 +0100

    begun working on google maps api implementation

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,7 @@ import React, {useState, useEffect} from "react";
 const App = () => {
 
 	return (
-		<div 
-			className='map-container' style={{ height: '94vh', width: '100%' }}>
-				{localStorage.getItem('splashScreenDismissed') ? null : <SplashScreenModal/>}
-				<Outlet/>
-				<SimpleMap/>
-			</div>
+    <h1>hello</h1>
 	)
 }
 
---
+++ client/src/components/Map.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import './map.css'
+
