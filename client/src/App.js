9f69e74bd565db74f76b85b8c67a38f9b3b2e80f
hamzaaslam2121 <hamzaaslam2121@gmail.com>
2023-07-22 20:37:05 +0100

    troubleshoot google maps module not found

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from "react";
+import Map from './components/Map.jsx'
+
 
 const App = () => {
 
--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,3 +2,40 @@ import React from 'react'
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import './map.css'
 
+const Map = () => {
+    const { isLoaded } = useLoadScript({
+      googleMapsApiKey: process.env.AIzaSyB_gADBq2dJuxcRKDwv8_N1b7CAFaQ5IY8,
+    });
+    const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+  
+    const onLoad = (map) => {
+        const bounds = new google.maps.LatLngBounds();
+        markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+        map.fitBounds(bounds);
+      };
+
+
+    return (
+      <div className="App">
+        {!isLoaded ? (
+          <h1>Loading...</h1>
+        ) : (
+          <GoogleMap
+            mapContainerClassName="map-container"
+            center={center}
+            zoom={50}
+            onLoad={onLoad}>
+          {markers.map(({ lat, lng }) => (
+            <Marker position={{ lat, lng }} />
+          ))}
+        </GoogleMap>
+      )}
+      </div>
+    );
+
+      
+  };
+  
+  export default App;
+
+  
